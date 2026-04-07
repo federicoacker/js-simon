18efe58db8d55d787083db4e188d8f28c6f77f52
federicoacker <acker.federico@gmail.com>
2026-04-07 13:53:12 +0200

    Finita la funzione che genera i numeri

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,8 +3,17 @@ const simonSays = [];
 function generateNumbers(){
     simonSays.length = 0;
     let number;
-    while(simonSays.length() < 5){
-        number = Math.floor(Math.Random * 100) + 1;
+    while(simonSays.length < 5){
+        number = Math.floor(Math.random() * 100) + 1;
         simonSays.push(number);
     }
+    for(let i = 0; i < simonSays.length; i++){
+        const currentValue = simonSays[i];
+        const inputElem = document.querySelector(`#number-${i+1}`);
+        inputElem.value=currentValue;
+    }
+}
+
+function formSubmitHandler(){
+
 }
